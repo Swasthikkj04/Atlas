e258f8aacbe59386dbe359f16949c8bdfe479361
swasthikkj04 <swasthikgowdakj@gmail.com>
2026-07-16 18:12:27 +0530

    feat(prisma): register Prisma module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { HealthModule } from './modules/health/health.module';
-
+import { PrismaModule } from './infrastructure/prisma/prisma.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -10,6 +10,7 @@ import { HealthModule } from './modules/health/health.module';
       cache: true,
       expandVariables: true,
     }),
+    PrismaModule,
     HealthModule,
   ],
 })
